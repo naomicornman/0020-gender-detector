16b55f5a41751893504fd373dc1d47df94591a2a
ncornman <ncornman@stanford.edu>
2016-03-03 13:08:01 -0800

    c.py

--- c.py
+++ c.py
@@ -2,11 +2,9 @@ from os.path import join, basename
 from glob import glob
 DATA_DIR = 'tempdata'
 
-# unlike b.py, this simply keeps a count of "names", not total babies
 tally = {'M': set(), 'F': set()}
 
 for fname in glob(join(DATA_DIR, '*.txt')):
-    # doing the filtering if filenames in one loop
     year = basename(fname)[3:7]
     if year >= "1950":
         for line in open(fname, 'r'):
