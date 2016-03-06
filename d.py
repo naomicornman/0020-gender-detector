f0fb3b0d42cc1f1afcd8296a4743ad5f20b3177c
ncornman <ncornman@stanford.edu>
2016-03-05 19:54:32 -0800

    d.py

--- d.py
+++ d.py
@@ -1,1 +1,16 @@
-d.py
+from os.path import join, basename
+from glob import glob
+DATA_DIR = 'tempdata'
+
+for fname in glob(join(DATA_DIR, '*.txt')):
+    year = basename(fname)[3:7]
+    tally = {'M': set(), 'F': set()}
+    if year >= "1950":
+    	print (year)
+    	for line in open(fname, 'r'):
+    		name, gender, babies = line.split(',')
+    		tally[gender].add(name)
+    	print("F:", len(tally['F']),
+			"M:", len(tally['M']))
+    	f_to_m_ratio = round(100 * len(tally['F']) / len(tally['M']))
+    	print("F/M baby ratio:", f_to_m_ratio)
